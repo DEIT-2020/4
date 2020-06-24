c06fa0cbb250a744322e9ae97cf82f30f8c285c2
周子骏 <1152433645@qq.com>
2020-06-24 22:20:08 +0800

    文件夹修改

--- heroes/lib/controller/commentcontroller.dart
+++ heroes/lib/controller/commentcontroller.dart
@@ -1,10 +1,21 @@
 import 'package:aqueduct/aqueduct.dart';
 import 'package:heroes/heroes.dart';
+import 'package:heroes/model/comment.dart';
+import 'package:heroes/model/user.dart';
 class CommentController extends ResourceController {
 
   CommentController(this.context);
 
   final ManagedContext context;
 
-  
+   @Operation.post() 
+  FutureOr<Response> insertArticle(
+      @Bind.body(ignore: ["createData"]) Comment comment) async {
+    
+    
+    
+    final insertedComment = await context.insertObject<Comment>(comment);
+    return Response.ok(insertedComment);
+  }
+
 }
