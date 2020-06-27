204959aae15d76b0d943e6dc997d8b3ee892aadb
li-hanyu <542147230@qq.com>
2020-06-27 09:44:19 +0800

    10184500220

---
+++ angular/web/search.dart
@@ -0,0 +1,8 @@
+import 'package:angular/angular.dart';
+import 'package:angular_dart/app_component.template.dart' as ng;
+import 'dart:html';
+import 'dart:convert';
+
+void main() {
+  runApp(ng.AppComponentNgFactory);
+}
